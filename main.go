2ae285054a9d0e3f8cd5dc35aab7d399e3ca19e0
Lukas <mail@lukas.contact>
2022-11-20 21:56:17 +0100

    can pass arguments by flag

--- main.go
+++ main.go
@@ -5,23 +5,60 @@ import (
 	"net/http"
 	"net"
 	"os"
+	"flag"
 	"os/signal"
 	"syscall"
+	"math/rand"
+	"time"
 
 	"github.com/AdguardTeam/gomitmproxy"
 
 )
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	port := flag.Int("p", 0, "port of the proxy")
+	user := flag.String("user", "", "username for the proxy basic auth")
+	pwd := flag.String("password", "", "password for the proxy basic auth")
+
+	flag.Parse()
+
+	if *user == "" {
+		*user = randSeq(10)
+		log.Printf("no user given using: %s", *user)
+	}
+
+	if *pwd == "" {
+		*pwd = randSeq(10)
+		log.Printf("no password given using: %s", *pwd)
+	}
+
+	if *port == 0 {
+		*port = 8080
+		log.Printf("no port given using: %d", *port)
+	}
+
+
 	proxy := gomitmproxy.NewProxy(gomitmproxy.Config{
 		ListenAddr: &net.TCPAddr{
 			IP:   net.IPv4(0, 0, 0, 0),
-			Port: 8080,
+			Port: *port,
 		},
 		OnRequest:  onRequest,
 		OnResponse: onResponse,
-		Username: "user",
-		Password: "pass",
+		Username: *user,
+		Password: *pwd,
 	})
 
 	err := proxy.Start()
